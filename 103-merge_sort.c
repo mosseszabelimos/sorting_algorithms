5b5daef70289ab3c719dd7821533f6cfa9cd82e2
Abel <abeldawit.h@gmail.com>
2023-04-25 11:33:32 +0000

    error fixed for task 103

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,132 +1,145 @@
-#include "sort.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
 
 /**
- * print_left_right - print left and right partitions
- * @array: array
- * @size: size of second array
- * @first: initial position
- * @mid: middle position
+ * copy - copies data from one buffer to another
+ *
+ * @src: source buffer
+ * @dst: destination buffer
+ * @size: size of buffers
+ *
+ * Return: No Return
  */
-void print_left_right(int *array, int size, int first, int mid)
-{
-	int k;
-
-	printf("Merging...\n");
-	printf("[left]: ");
-	k = first;
-	while (k < mid)
-	{
-		if (k != mid - 1)
-			printf("%d, ", array[k]);
-		else
-			printf("%d\n", array[k]);
-		k++;
-	}
-
-	printf("[right]: ");
-	k = mid;
-	while (k < size)
-	{
-		if (k < size - 1)
-			printf("%d, ", array[k]);
-		else
-			printf("%d\n", array[k]);
-		k++;
-	}
-}
-
-/**
- * merge - merge the values in the position of array
- * @array: first array
- * @size: size of second array
- * @cpy: copy of array
- * @first: initial position
- * @mid: middle position
- * first one of the second array
- */
-void merge(int *array, int size, int first, int mid, int *cpy)
-{
-	int i, j, k;
-
-	print_left_right(array, size, first, mid);
-
-	i = first;
-	j = mid;
-
-	printf("[Done]: ");
-	k = first;
-	while (k < size)
-	{
-		if (i < mid && (j >= size || array[i] <= array[j]))
-		{
-			cpy[k] = array[i];
-			i++;
-		}
-		else
-		{
-			cpy[k] = array[j];
-			j++;
-		}
-		if (k < size - 1)
-			printf("%d, ", cpy[k]);
-		else
-			printf("%d\n", cpy[k]);
-		k++;
-	}
-}
-/**
- * mergeSort - array separator
- * @cpy: copy of array
- * @first: initial position
- * @size: size of the original  array
- * @array: the original array
- */
-void mergeSort(int *cpy, int first, int size, int *array)
-{
-	int mid;
-
-	if (size - first < 2)
-		return;
-
-	mid = (size + first) / 2;
-
-	mergeSort(array, first, mid, cpy);
-	mergeSort(array, mid, size, cpy);
-
-	merge(cpy, size, first, mid, array);
-}
-/**
- * copy_array - copy array of int
- * @arr: array src
- * @cpy: array dest
- * @size : array size
- */
-void copy_array(int *arr, int *cpy, int size)
+void copy(int *src, int *dst, int size)
 {
 	int i;
 
-	for (i = 0; i < (int)size; i++)
-		cpy[i] = arr[i];
+	for (i = 0; i < size; i++)
+		dst[i] = src[i];
 }
-
 /**
- * merge_sort - create partition and copy
- * @array: array
- * @size : array size
+ * merge - merges two sets of data in ascending order
+ * but they must already be sorted before hand
+ * @array: first set of data
+ * @buff: second set of data
+ * @minL: lower range of first set of data
+ * @maxL: upper range of first set of data
+ * @minR: lower range of second set of data
+ * @maxR: upper range of second set of data
+ *
+ * Return: No Return
+ */
+void merge(int *array, int *buff, int minL, int maxL, int minR, int maxR)
+{
+	int i = minL, j = minR, k = minL;
+
+	while (i <= maxL || j <= maxR)
+
+		if (i <= maxL && j <= maxR)
+			if (buff[i] <= buff[j])
+				array[k] = buff[i], k++, i++;
+			else
+				array[k] = buff[j], k++, j++;
+
+		else if (i > maxL && j <= maxR)
+			array[k] = buff[j], k++, j++;
+		else
+			array[k] = buff[i], k++, i++;
+}
+/**
+ * printcheck - prints an array in a given range
+ *
+ * @array: array of data to be print
+ * @r1: start of range
+ * @r2: end of range
+ *
+ * Return: No Return
+ */
+void printcheck(int *array, int r1, int r2)
+{
+	int i;
+
+	for (i = r1; i <= r2; i++)
+	{
+		if (i > r1)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+	printf("\n");
+}
+/**
+ * split - recursive function to split data into merge tree
+ *
+ * @array: array of data to be split
+ * @buff: auxiliary array of data for merging
+ * @min: min range of data in array
+ * @max: max range of data in array
+ * @size: size of total data
+ *
+ * Return: No Return
+ */
+void split(int *array, int *buff, int min, int max, int size)
+{
+	int mid, tmax, minL, maxL, minR, maxR;
+
+	if ((max - min) <= 0)
+		return;
+
+	mid = (max + min + 1) / 2;
+	tmax = max;
+	max = mid - 1;
+
+	minL = min;
+	maxL = max;
+
+	split(array, buff, min, max, size);
+
+	min = mid;
+	max = tmax;
+
+	minR = min;
+	maxR = max;
+
+	split(array, buff, min, max, size);
+
+	printf("Merging...\n");
+	printf("[left]: ");
+
+	printcheck(array, minL, maxL);
+
+	printf("[right]: ");
+
+	printcheck(array, minR, maxR);
+	merge(array, buff, minL, maxL, minR, maxR);
+	copy(array, buff, size);
+
+	printf("[Done]: ");
+	printcheck(array, minL, maxR);
+}
+/**
+ * merge_sort - sorts an array of integers in ascending order
+ * using the Merge sort algorithm
+ *
+ * @array: array of data to be sorted
+ * @size: size of data
+ *
+ * Return: No Return
  */
 void merge_sort(int *array, size_t size)
 {
-	int *cpy;
+	int *buff;
 
-	cpy = malloc(sizeof(int) * size - 1);
-
-	if (cpy == NULL)
+	if (size < 2)
 		return;
 
-	copy_array(array, cpy, size);
+	buff = malloc(sizeof(int) * size);
+	if (buff == NULL)
+		return;
 
-	mergeSort(cpy, 0, size, array);
-	free(cpy);
+	copy(array, buff, size);
+
+	split(array, buff, 0, size - 1, size);
+
+	free(buff);
 }
